9c39f844d8dfd53731b91a466deb2d7237718215
www-coder-in <www.coder.in@gmail.com>
2015-01-22 17:05:44 -0800

    all

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+require 'bcrypt'
+
+class User < ActiveRecord::Base
+	include BCrypt
+	has_many :surveys
+	attr_accessor :password
+	before_save :password
+	validates :username, uniqueness: true
+	
+
+	def password
+		@password ||= Password.new(password_hash)
+	end
+
+	def password=(new_password)
+		@password = Password.create(new_password)
+		self.password_hash = @password
+	end
+
+
+end
